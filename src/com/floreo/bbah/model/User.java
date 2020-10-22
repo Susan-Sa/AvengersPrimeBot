e162095cf6ec0c0eeb7cf5274f8855cef4d44327
Rhema Dapaah <rddaps@gmail.com>
2020-10-22 15:31:27 -0500

    User TODO completed

--- src/com/floreo/bbah/model/User.java
+++ src/com/floreo/bbah/model/User.java
@@ -10,25 +10,93 @@ import de.ralleytn.simple.json.JSONObject;
  */
 
 public class User {
+    private Object profile = new Object();
 
-    // TODO: implement private fields for each of the following user JSON keys:
-    // "id"
-    // "name"
-    // "deleted"
-    // "color"
-    // "profile"
-    // "is_admin"
-    // "is_owner"
-    // "is_primary_owner"
-    // "is_restricted"
-    // "is_ultra_restricted"
-    // "has_2fa"
-    // "two_factor_type"
-    // "has_files"
+    private String id;
+    private String name;
+    private boolean deleted;
+    private String color;
+    private boolean is_admin;
+    private boolean is_owner;
+    private boolean is_primary_owner;
+    private boolean is_restricted;
+    private boolean is_ultra_restricted;
+    private boolean has_2fa;
+    private String two_factor_type;
+    private boolean has_files;
 
     public User(JSONObject json) {
-        // TODO: parse a user from the incoming json
+
+    if (json.get("id") != null){
+        this.id = (String) json.get("id");
     }
 
-    // TODO add getters to access private fields
+    if (json.get("name") != null){
+        this.name = (String) json.get("name");
+    }
+
+    if (json.get("deleted") != null){
+        this.deleted = (Boolean) json.get("deleted");
+    }
+
+    if (json.get("color") != null){
+        this.color =(String) json.get("color");
+    }
+
+    if (json.get("is_admin") != null){
+        this.is_admin = (Boolean) json.get("is_admin");
+    }
+
+    if (json.get("is_owner") != null){
+        this.is_owner = (Boolean) json.get("is_owner");
+    }
+
+    if (json.get("is_primary_owner") != null){
+        this.is_primary_owner = (Boolean) json.get("is_primary_owner");
+    }
+
+    if (json.get("is_restricted") != null){
+        this.is_restricted = (Boolean) json.get("is_restricted");
+    }
+
+    if (json.get("is_ultra_restricted") != null){
+        this.is_ultra_restricted = (Boolean) json.get("is_ultra_restricted");
+    }
+
+    if(json.get("has_2fa") != null){
+        this.has_2fa = (Boolean) json.get("has_2fa");
+    }
+
+    if(json.get("two_factor_type") != null){
+        this.two_factor_type = (String) json.get("two_factor_type");
+    }
+
+    if(json.get("has_files") != null){
+        this.has_files = (Boolean) json.get("has_files");
+    }
+}
+
+    public String getId() { return id; }
+
+    public String getName() {return name; }
+
+    public boolean getDeleted() { return deleted; }
+
+    public String getColor() { return color; }
+
+    public boolean getIs_admin() { return is_admin; }
+
+    public boolean getIs_owner() { return is_owner; }
+
+    public boolean getIs_primary_owner() { return is_primary_owner; }
+
+    public boolean getIsRestricted() { return is_restricted; }
+
+    public boolean getIs_ultra_restricted() { return is_ultra_restricted; }
+
+    public boolean getHas_2fa() { return has_2fa; }
+
+    public String getTwoFactorType() { return two_factor_type; }
+
+    public boolean getHas_files() { return has_files; }
 }
